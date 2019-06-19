96e5d141cf76a0cc9db0311304c6daaff26ae48a
Kamil Koci <kamil.koci@cloveretl.com>
2019-06-19 09:21:31 +0200

    UPDATE: CLO-16488 - Fix problem with JMX SSL

--- tomcat/bin/setenv.sh
+++ tomcat/bin/setenv.sh
@@ -9,27 +9,8 @@ export CATALINA_OPTS="$CATALINA_OPTS -XX:InitiatingHeapOccupancyPercent=30"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:G1ReservePercent=10"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxGCPauseMillis=100"
  
-#echo "RMI_HOSTNAME - ${RMI_HOSTNAME}"
 export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.config.file=$CLOVER_HOME_CONF_DIR/jmx-conf.properties"
-
-#JMX settings
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote=true"
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.port=8686"
-
-#Settings JMX without SSL
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.rmi.port=8686"
-#export CATALINA_OPTS="$CATALINA_OPTS -Djava.rmi.server.hostname=${RMI_HOSTNAME}"
-		
-#Settings JMX with SSL
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl=true"
-#export CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote.ssl.need.client.auth=false"
-#export CATALINA_OPTS="$CATALINA_OPTS -Djavax.net.ssl.keyStore=/var/clover/conf/serverKS.jks"
-#export CATALINA_OPTS="$CATALINA_OPTS -Djavax.net.ssl.keyStorePassword=semafor"
-
-
+export CATALINA_OPTS="$CATALINA_OPTS -Djava.rmi.server.hostname=${RMI_HOSTNAME}"
 
 export CATALINA_OPTS="$CATALINA_OPTS -Dclover.default.config.file=$DEFAULT_CFG_FILE"
 export CATALINA_OPTS="$CATALINA_OPTS -Dclover.config.file=$CUSTOM_CFG_FILE"

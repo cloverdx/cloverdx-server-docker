135fce95cd895df1404b5485fc87fb4b6d08c37c
Milan Křivánek <milan.krivanek@cloveretl.com>
2019-08-20 09:43:32 +0200

    UPDATE: CLO-17139 - Kubernetes: run.sh and README.md added, echo.rjob simplified - Reviewer: kocik, reichmanf

---
+++ examples/kubernetes/README.md
@@ -0,0 +1,24 @@
+# Kubernetes Deployment Example
+
+This example runs CloverDX Server with PostreSQL database as a deployment in Kubernetes.
+It contains a sample echo data service, which simply prints the string passed as a path parameter.
+
+### Requirements
+* bash + envsubst
+* Docker + docker CLI
+* Kubernetes + kubectl
+* Docker image registry accessible from Kubernetes
+* ``license.dat``
+
+---
+
+### Running the example
+
+* Put your ``license.dat`` file to ``examples/kubernetes`` directory.
+* Execute `run.sh` and pass the hostname and port of your Docker registry as a parameter. The script will deploy the example and start port forwarding to localhost:8090.
+
+    ```
+    ./run.sh my-docker-registry:5000
+    ```
+
+The data service is now available at <http://localhost:8090/clover/data-service/echo/Hello+World!>.
---
+++ examples/kubernetes/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+DOCKER_REGISTRY=$1
+if [ -z $DOCKER_REGISTRY ]; then
+	echo "Usage: $0 my-docker-registry:5000"
+	exit 1
+fi
+
+# Build and push the base image
+echo "Building the base image"
+BASE_IMAGE_TAG=$DOCKER_REGISTRY/cloverdx-server:latest
+docker build -t $BASE_IMAGE_TAG ../..
+docker push $BASE_IMAGE_TAG
+
+echo "Building the example"
+# Build and push the image containing the example
+EXAMPLE_TAG=$DOCKER_REGISTRY/cloverdx-kubernetes-example:latest
+docker build --build-arg DOCKER_REGISTRY=$DOCKER_REGISTRY -t $EXAMPLE_TAG .
+docker push $EXAMPLE_TAG
+
+# Constants
+NAMESPACE=cloverdx
+DEPLOYMENT_NAME=cloverdx-example
+SERVICE_NAME=cloverdx-svc
+
+echo "Deploying the example to Kubernetes"
+# Create and expose the deployment as a service
+cat cloverdx.yaml | envsubst '$DOCKER_REGISTRY' | kubectl apply -f -
+echo "Waiting for service startup"
+kubectl wait --for=condition=available --timeout=60s --namespace=$NAMESPACE deployment/$DEPLOYMENT_NAME
+
+# Print service description
+kubectl get services --namespace=$NAMESPACE
+
+# Start port forwarding to localhost:8090
+kubectl port-forward --namespace=$NAMESPACE svc/$SERVICE_NAME 8090:8080

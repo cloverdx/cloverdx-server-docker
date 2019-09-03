7d35a4449eb4750796f1384a592b20106f972a87
Filip Reichman <filip.reichman@cloveretl.com>
2019-09-03 13:36:09 +0200

    UPDATE: CLO-17268 - gravitee containers split to pods

--- examples/kubernetes/run.sh
+++ examples/kubernetes/run.sh
@@ -22,7 +22,7 @@ docker build --build-arg DOCKER_REGISTRY=$DOCKER_REGISTRY -t $EXAMPLE_TAG .
 docker push $EXAMPLE_TAG
 
 # Constants
-NAMESPACE=cloverdx
+NAMESPACE=filip
 DEPLOYMENT_NAME=cloverdx-example
 SERVICE_NAME=cloverdx
 
@@ -41,11 +41,28 @@ echo "Create and expose monitoring";
 # kubectl apply -f cloverdx-monitoring.yaml --namespace=$NAMESPACE
 
 echo "Waiting for monitoring service startup"
-kubectl wait --for=condition=available --timeout=60s --namespace=$NAMESPACE deployment/prometheus
-kubectl wait --for=condition=available --timeout=60s --namespace=$NAMESPACE deployment/grafana
+#kubectl wait --for=condition=available --timeout=60s --namespace=$NAMESPACE deployment/prometheus
+#kubectl wait --for=condition=available --timeout=60s --namespace=$NAMESPACE deployment/grafana
+
+kubectl create --namespace=$NAMESPACE -f elasticsearch.yaml 
+kubectl create --namespace=$NAMESPACE -f mongodb.yaml
+
+kubectl create --namespace=$NAMESPACE -f gravitee-gateway.yaml
+kubectl create --namespace=$NAMESPACE -f gravitee-management-api.yaml
+kubectl create --namespace=$NAMESPACE -f gravitee-management-ui.yaml
+
+kubectl create --namespace=$NAMESPACE -f gravitee-am-gateway.yaml
+kubectl create --namespace=$NAMESPACE -f gravitee-am-management-api.yaml
+kubectl create --namespace=$NAMESPACE -f gravitee-am-management-ui.yaml
 
 # Print service description
 kubectl get services --namespace=$NAMESPACE
+
+kubectl port-forward --namespace=$NAMESPACE svc/gravitee-management-api-svc 8083:8083 &
+
+kubectl port-forward --namespace=$NAMESPACE svc/gravitee-am-management-api-svc 8093:8093 &
+
+kubectl port-forward --namespace=$NAMESPACE svc/gravitee-am-management-ui-svc 81:81 &
 
 # Start port forwarding to localhost:8090
 kubectl port-forward --namespace=$NAMESPACE svc/$SERVICE_NAME 8090:8080
